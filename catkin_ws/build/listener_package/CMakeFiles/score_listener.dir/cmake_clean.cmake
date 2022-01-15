c30667d0d8fc87c1c942cf17a36b11cbb575ddab
anotheruser1458 <maxwellpauly32@gmail.com>
2022-01-14 17:26:25 -1000

    initial commit

---
+++ catkin_ws/build/listener_package/CMakeFiles/score_listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/maxwell/Desktop/catkin_ws/devel/lib/listener_package/score_listener"
+  "/home/maxwell/Desktop/catkin_ws/devel/lib/listener_package/score_listener.pdb"
+  "CMakeFiles/score_listener.dir/src/score_listener.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/score_listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
