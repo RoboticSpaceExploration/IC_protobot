aa82810b02566fa2d3af01d89850689643165035
anotheruser1458 <maxwellpauly32@gmail.com>
2022-02-03 17:46:21 -1000

    repo fix

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/maxwell/Desktop/IC_protobot/catkin_ws/src
+# Build directory: /home/maxwell/Desktop/IC_protobot/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("listener_package")
+subdirs("my_roscpp_library")
+subdirs("async_web_server_cpp")
+subdirs("video_stream_opencv")
+subdirs("web_video_server")
