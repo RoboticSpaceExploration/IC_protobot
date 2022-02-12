8ca855b253f26142fa788be1ef5654f411d86ea3
anotheruser1458 <maxwellpauly32@gmail.com>
2022-02-11 18:33:33 -1000

    navigation stack added

---
+++ catkin_ws/build/nav_test/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/maxwell/Desktop/IC_protobot/catkin_ws/src/nav_test/include".split(';') if "/home/maxwell/Desktop/IC_protobot/catkin_ws/src/nav_test/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "nav_msgs;roscpp".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "nav_test"
+PROJECT_SPACE_DIR = "/home/maxwell/Desktop/IC_protobot/catkin_ws/devel"
+PROJECT_VERSION = "0.0.0"
